bb80e13c4abcf494c1081f7c2dc831d64d3a5950
inkogniito <120864571+inkogniito@users.noreply.github.com>
2024-11-09 11:12:38 +0300

    Delete build/Desktop_Qt_6_7_3_MSVC2022_64bit-Debug directory

--- build/Desktop_Qt_6_7_3_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_221_329_Dorohov_Exam_FILE C:/Users/dfusr/Desktop/221_329_Dorohov_Exam/build/Desktop_Qt_6_7_3_MSVC2022_64bit-Debug/221_329_Dorohov_Exam.exe)
-set(__QT_DEPLOY_TARGET_221_329_Dorohov_Exam_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_221_329_Dorohov_Exam_RUNTIME_DLLS C:/Qt/6.7.3/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.3/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.7.3/msvc2022_64/bin/Qt6Cored.dll)
